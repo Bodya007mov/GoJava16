d9918173153f347b6d8fdf88305a332564bf166c
Bogdan <movchanbogdan1@gmail.com>
2020-06-15 14:27:14 +0300

    added task2

--- homework11/src/main/java/library/Person.java
+++ homework11/src/main/java/library/Person.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Semaphore;
 public class Person implements Runnable{
 
     public static Semaphore semaphore;
+    private final Semaphore doorSemaphore = new Semaphore(1);
     private final int PERSON_NUMBER;
 
     public Person(int personNumber) {
@@ -15,13 +16,25 @@ public class Person implements Runnable{
         System.out.printf("Посетитель %d пришел ко входу в библиотеку \n", PERSON_NUMBER);
         try {
             if(semaphore.availablePermits() == 0) {
-                System.out.printf("Посетитель %d ЖДЕТ входа в библиотеку \n", PERSON_NUMBER);
+                System.out.printf("Посетитель %d ждет входа в библиотеку \n", PERSON_NUMBER);
             }
             semaphore.acquire();
+            System.out.printf("Посетитель %d подошел к двери с улицы \n", PERSON_NUMBER);
+            doorSemaphore.acquire();
+            System.out.printf("Посетитель %d проходит через дверь внутрь \n", PERSON_NUMBER);
+            Thread.sleep(500);
+            doorSemaphore.release();
+            System.out.printf("Посетитель %d прошел через дверь внутрь \n", PERSON_NUMBER);
             System.out.printf("Посетитель %d вошел в библиотеку \n", PERSON_NUMBER);
             Thread.sleep(1000);
             System.out.printf("Посетитель %d читает книгу \n", PERSON_NUMBER);
             Thread.sleep((long) (Math.random() * 5000 + 1000));
+            System.out.printf("Посетитель %d подошел к двери изнутри \n", PERSON_NUMBER);
+            doorSemaphore.acquire();
+            System.out.printf("Посетитель %d проходит через дверь наружу \n", PERSON_NUMBER);
+            Thread.sleep(500);
+            doorSemaphore.release();
+            System.out.printf("Посетитель %d прошел через дверь наружу \n", PERSON_NUMBER);
             semaphore.release();
             System.out.printf("Посетитель %d покинул библиотеку \n", PERSON_NUMBER);
         } catch (InterruptedException e) {
